e97d4563dc4c947e31b798437c9a151708d8c738
Cameron Klein <cameron.klein@gmail.com>
2014-10-27 10:07:40 -0700

    Initial Commit

---
+++ Hacker Mission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hacker Mission
+//
+//  Created by Cameron Klein on 10/27/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
