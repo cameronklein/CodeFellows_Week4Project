1333942c8a5810f2993167da798d511208df6f43
Cameron Klein <cameron.klein@gmail.com>
2014-10-27 13:51:24 -0700

    add leadgamecontroller class

---
+++ Hacker Mission/GameController.swift
@@ -0,0 +1,26 @@
+//
+//  GameBrain.swift
+//  Hacker Mission
+//
+//  Created by Cameron Klein on 10/27/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+import Foundation
+
+class LeadGameController {
+  
+  var game : Game
+
+  init(){
+    
+  }
+  
+  func gatherPlayers(){
+    
+    
+  }
+  
+  
+  
+}
