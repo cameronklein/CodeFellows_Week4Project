1905c23b7c84b2db6b38a9f6e4b3b268e3aaa0ae
natebirkholz <nbirkholz@gmail.com>
2014-10-29 12:01:25 -0700

    Added NSCoding./NSKeyedArchiver functionality for the network operations.

--- Hacker Mission/GameSession.swift
+++ Hacker Mission/GameSession.swift
@@ -13,8 +13,10 @@ class GameSession {
     var players : NSMutableArray // Array of Player
     var missions : NSMutableArray // Array of Mission
     var leader : Player? // Get from players.objectAtIndex(Int)
-    var currentMission : Int? // Get from missions.objectAtIndex(Int)
+    var currentMission : NSInteger? // Get from missions.objectAtIndex(Int)
     var currentGameState  : GameEvent?
+    var failedMissionCount : NSInteger = 0
+    var passedMissionCount : NSInteger = 0
 
 
 
@@ -24,6 +26,37 @@ class GameSession {
 
     }
 
+    required init(coder aDecoder: NSCoder) {
+
+        self.players = aDecoder.decodeObjectForKey("players") as NSMutableArray
+        self.missions = aDecoder.decodeObjectForKey("missions") as NSMutableArray
+        self.leader = aDecoder.decodeObjectForKey("leader") as Player?
+        let currentMissionFor = NSInteger(aDecoder.decodeIntForKey("currentMission"))
+        self.currentMission = currentMissionFor
+        let currentGameStateFor = aDecoder.decodeObjectForKey("currentGameState") as GameEvent.RawValue!
+        var currentGameStateIs = GameEvent(rawValue: currentGameStateFor)
+        self.currentGameState = currentGameStateIs
+        self.failedMissionCount = aDecoder.decodeIntegerForKey("failedMissionCount") as NSInteger
+        self.passedMissionCount = aDecoder.decodeIntegerForKey("passedMissionCount") as NSInteger
+
+    }
+
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.players, forKey: "players")
+        aCoder.encodeObject(self.missions, forKey: "missions")
+        aCoder.encodeInteger(self.failedMissionCount, forKey: "failedMissionCount")
+        aCoder.encodeInteger(self.passedMissionCount, forKey: "passedMissionCount")
+        if self.leader != nil {
+            aCoder.encodeObject(self.leader, forKey: "leader")
+        }
+        if self.currentMission != nil {
+            aCoder.encodeObject(self.currentMission, forKey: "currentMission")
+        }
+        if self.currentGameState != nil {
+            aCoder.encodeObject(self.currentGameState?.rawValue, forKey: "currentGameState")
+        }
+
+    }
 
     class func populateMissionList() -> NSMutableArray {
         var missionList : NSMutableArray?
@@ -44,6 +77,21 @@ class GameSession {
         let gameIDHash = playerForHash.hash as NSInteger!
 
         return gameIDHash as NSInteger!
+    }
+
+    class func wrapGameSession(object: UserInfo) -> NSMutableData {
+
+        var passGame = NSMutableData()
+        passGame = NSKeyedArchiver.archivedDataWithRootObject(object) as NSMutableData
+
+        return passGame
+    }
+
+    class func unwrapGameSession(object: NSMutableData) -> UserInfo {
+        var passedGame = NSKeyedUnarchiver.unarchiveObjectWithData(object) as UserInfo
+
+        return passedGame as UserInfo
+        
     }
     
     
